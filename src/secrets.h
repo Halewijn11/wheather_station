ea4840520eaa908033a6be7e14f305279ada40f0
Halewijn <halewijn.vandenbossche@gmail.com>
2025-12-05 19:20:10 +0100

    initial commit

---
+++ src/secrets.h
@@ -0,0 +1,19 @@
+// wifi at abdijstraat
+#define SECRET_SSID "telenet-8E11184"
+#define SECRET_PASS "Bc6varmbkpmQ"
+// wifi at fuutlaan
+// #define SECRET_SSID "Fuutgroep6!"
+// #define SECRET_PASS "144"
+
+
+// channel on thingspeak
+#define SECRET_CH_ID 3194176
+#define SECRET_WRITE_APIKEY "83UGA9THWKAMJLVZ"
+
+
+// const char* ssid = "144";
+// const char* password = "Fuutgroep6!";
+// #define SECRET_SSID "your_wifi_name"
+// #define SECRET_PASS "your_wifi_password"
+// #define SECRET_CH_ID 123456
+// #define SECRET_WRITE_APIKEY "YOUR_API_KEY"
