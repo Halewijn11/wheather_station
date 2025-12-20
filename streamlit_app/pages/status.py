61aa9ba30290953cd8b3fd85b6e41bf4f3f7d71e
Halewijn <halewijn.vandenbossche@gmail.com>
2025-12-20 12:09:42 +0100

    update to streamlit app

--- streamlit_app/pages/status.py
+++ streamlit_app/pages/status.py
@@ -19,7 +19,8 @@ st.title("Status")
 google_sheet_df = utils.get_google_sheet_df()
 df = utils.tidy_google_sheet_df(google_sheet_df)
 last_measurement_string =  utils.get_last_measurement_string(df)
-
+time_window_df = df.tail(50)
+time_window_df.to_csv('delete.csv')
 # #--------------------- building the streamlit app -----------------------------
 # --- Top row ---
 # top_cols = st.columns(3)
@@ -35,12 +36,12 @@ st.info(f"Time since last measurement: **{last_measurement_string}**")
 
 # Layout
 col1, col2 = st.columns([1, 1])
-latest = df["uplink_message_rx_metadata_0_channel_rssi"].iloc[-1]
+latest = time_window_df["uplink_message_rx_metadata_0_channel_rssi"].iloc[-1]
 with col1:
     st.metric("last RSSI", f"{latest:.1f} dBm")
 
 with col2:
-    spark = alt.Chart(df.head(50)).mark_line().encode(
+    spark = alt.Chart(time_window_df.tail(50)).mark_line().encode(
         x=alt.X("received_at", axis=None),
         y=alt.Y(
             "uplink_message_rx_metadata_0_channel_rssi",
@@ -50,8 +51,8 @@ with col2:
                     title="rssi",
                 ),
             scale=alt.Scale(domain=[
-                df["uplink_message_rx_metadata_0_channel_rssi"].min(),
-                df["uplink_message_rx_metadata_0_channel_rssi"].max()
+                time_window_df["uplink_message_rx_metadata_0_channel_rssi"].min(),
+                time_window_df["uplink_message_rx_metadata_0_channel_rssi"].max()
             ])
         )
     ).properties(height=100)
@@ -59,12 +60,12 @@ with col2:
 
 
 col1, col2 = st.columns([1, 1])
-latest = df["uplink_message_rx_metadata_0_snr"].iloc[-1]
+latest = time_window_df["uplink_message_rx_metadata_0_snr"].iloc[-1]
 with col1:
     st.metric("last SNR", f"{latest:.1f}")
 
 with col2:
-    spark = alt.Chart(df.head(50)).mark_line().encode(
+    spark = alt.Chart(time_window_df.tail(50)).mark_line().encode(
         x=alt.X("received_at", axis=None),
         y=alt.Y(
             "uplink_message_rx_metadata_0_snr",
@@ -74,9 +75,10 @@ with col2:
                     title="rssi",
                 ),
             scale=alt.Scale(domain=[
-                df["uplink_message_rx_metadata_0_snr"].min(),
-                df["uplink_message_rx_metadata_0_snr"].max()
+                time_window_df["uplink_message_rx_metadata_0_snr"].min(),
+                time_window_df["uplink_message_rx_metadata_0_snr"].max()
             ])
         )
     ).properties(height=100)
     st.altair_chart(spark, use_container_width=True)
+    
