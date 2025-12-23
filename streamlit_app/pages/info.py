ae25ce6f869effe3906274a52271358f62559dd2
Halewijn <halewijn.vandenbossche@gmail.com>
2025-12-23 19:17:30 +0100

    changes to site

--- streamlit_app/pages/info.py
+++ streamlit_app/pages/info.py
@@ -1,1 +1,10 @@
 #here maybe past some links etc
+
+import streamlit as st
+
+url = "https://docs.google.com/spreadsheets/d/1zPwrfEDDBZVqb3mwbBCHdeCaGAHnUresvGlHDXuD_qI/edit?usp=sharing"
+st.write("The raw data of this project can be found back [in this google sheet](%s)." % url)
+# st.markdown("check out this [link](%s)" % url)
+# """
+# [**Vega-Lite examples.**](https://vega.github.io/vega-lite/examples/)
+# """
