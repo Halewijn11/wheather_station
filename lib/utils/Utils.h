61ad8e968085c94aa38d0005f956a5b5f26c5ed3
Halewijn <halewijn.vandenbossche@gmail.com>
2026-01-03 12:03:37 +0100

    changes to read the power meter

--- lib/utils/Utils.h
+++ lib/utils/Utils.h
@@ -4,9 +4,22 @@
 
 extern volatile unsigned int fan_pulse_count;
 
+
+
 int readFanSpeed();
 void fan_Counter();
 int readFanSpeed_Updated(int tach_pin);
 int percentage_To_Pwm(int percentage); 
+
+//for the ina3221 readings
+// 🔹 Forward declaration (NO include needed here)
+class Adafruit_INA3221;
+struct Ina3221Reading {
+  float voltage_V;
+  float current_mA;
+  float power_mW;
+};
+
+Ina3221Reading readIna3221Channel(Adafruit_INA3221& ina3221, uint8_t channel);
 
 #endif
--- src/cubecell_inn3221_improved.cpp
+++ src/cubecell_inn3221_improved.cpp
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include "Adafruit_INA3221.h"
 #include <Wire.h>
+#include "Utils.h"
 
 // Create an INA3221 object
 Adafruit_INA3221 ina3221;
@@ -13,14 +14,16 @@ void setup() {
 
   Serial.println("Adafruit INA3221 simple test");
 
+  /****************************************************
+   * ina3221 setup
+   ****************************************************/
   // Initialize the INA3221
   if (!ina3221.begin(0x40, &Wire)) { // can use other I2C addresses or buses
     Serial.println("Failed to find INA3221 chip");
     while (1)
       delay(10);
   }
-  Serial.println("INA3221 Found!");
-
+  // Serial.println("INA3221 Found!");
   ina3221.setAveragingMode(INA3221_AVG_16_SAMPLES);
 
   // Set shunt resistances for all channels to 0.05 ohms
@@ -30,26 +33,24 @@ void setup() {
 
   // Set a power valid alert to tell us if ALL channels are between the two
   // limits:
-  ina3221.setPowerValidLimits(3.0 /* lower limit */, 15.0 /* upper limit */);
+  ina3221.setPowerValidLimits(2.0 /* lower limit */, 15.0 /* upper limit */);
 }
 
 void loop() {
   // Display voltage and current (in mA) for all three channels
-  for (uint8_t i = 0; i < 3; i++) {
-    float voltage = ina3221.getBusVoltage(i);
-    float current_mA = ina3221.getCurrentAmps(i) * 1000; // Convert to mA
-    float power_mW = voltage * (current_mA); // Power in milliwats
-
-    Serial.print("Channel ");
-    Serial.print(i);
+  Ina3221Reading r = readIna3221Channel(ina3221, 0);
+  float voltage_V = r.voltage_V;
+  float current_mA = r.current_mA;
+  float power_mW = r.power_mW;
+    // Serial.print("Channel ");
+    // Serial.print(i);
     Serial.print(": Voltage = ");
-    Serial.print(voltage, 2);
+    Serial.print(voltage_V, 2);
     Serial.print(" V, Current = ");
     Serial.print(current_mA, 2);
     Serial.print(" mA, Power = ");
     Serial.print(power_mW, 2);
     Serial.println(" mW");
-  }
 
   Serial.println();
   // Delay for 250ms before the next reading
