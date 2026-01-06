4718139c5b0eaae710c44e3350e5e1d191cc58c8
Halewijn <halewijn.vandenbossche@gmail.com>
2026-01-06 22:55:46 +0100

    addition of sunrise and sunset

--- streamlit_app/pages/dashboard.py
+++ streamlit_app/pages/dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import importlib
+import pandas as pd
 import utils
 importlib.reload(utils)
 from streamlit_extras.metric_cards import style_metric_cards
@@ -17,10 +18,32 @@ google_sheet_df = utils.get_google_sheet_df(sheet_gid=gid)
 if debug == True:
     st.write("Available columns in Sheet:", google_sheet_df.columns.tolist()) # Add this line
 
+
+
 df = utils.tidy_google_sheet_df(google_sheet_df,decoded_payload_data_col_name_list=[])
+# df  = pd.read_csv('data.csv')
 time_window_df = df.tail(50)
 
 
+# #--------------------- sunset and sunrise -----------------------------
+sunrise_str, sunset_str = utils.get_sunrise_sunset()
+# 1. We keep your column structure to limit the width
+col1, col2, col3, col4, buffer = st.columns([6, 9, 6, 9, 30])
+
+with col1:
+    st.image('./assets/sunrise.png', width=50)
+with col2:
+    # Use <br> instead of \n\n to remove the paragraph gap
+    st.markdown(f"**Sunrise**<br>{sunrise_str}", unsafe_allow_html=True)
+
+with col3:
+    st.image('./assets/sunset.png', width=50)
+with col4:
+    # Applying the same fix here
+    st.markdown(f"**Sunset**<br>{sunset_str}", unsafe_allow_html=True)
+
+with buffer:
+    pass
 # #--------------------- temperature -----------------------------
 # temperature_colname = utils.get_full_payload_colname('temperature')
 temperature_colname = 'temp_avg'
